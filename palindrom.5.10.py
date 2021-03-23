ce8af3a71175526965a21148a848be0075f852d1
KKFKristof <korcsmaroskristof-2020-c@cserhati-edu.hu>
2021-03-23 16:14:24 +0100

    eddig jutottam

---
+++ palindrom.5.10.py
@@ -0,0 +1,6 @@
+a = str(input("Adj meg egy karaktersorozat, én megmondom hogy palindrom e!: "))
+alap: str = ""
+for cv in range(len(alap)-1, -1, -1):
+    print("Ez palindrom!")
+else:
+    print("Ez nem az!")
