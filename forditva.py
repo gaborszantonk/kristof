e6f2715ca1162e5e39f48a73abba61b3ed6c47ac
KKFKristof <korcsmaroskristof-2020-c@cserhati-edu.hu>
2021-03-24 09:28:15 +0100

    oksi

--- forditva.py
+++ forditva.py
@@ -1,3 +1,10 @@
 a = input("Adj megy egy karakter sorozatot és én kiírom visszafele!")
+print(a)
+b: str = ""
 for cv in range(len(a)-1,  -1, -1):
-    print(a[cv])
+    b = b + a[cv]
+print(b)
+if a == b:
+    print("Ez plaindrom!")
+else:
+    print("Ez nem az!")
