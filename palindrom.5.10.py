cbd4cb9dad69ae20bc4d3f586b4197d37bb6baca
KKFKristof <korcsmaroskristof-2020-c@cserhati-edu.hu>
2021-03-24 09:28:49 +0100

    .

--- palindrom.5.10.py
+++
@@ -1,6 +0,0 @@
-a = str(input("Adj meg egy karaktersorozat, én megmondom hogy palindrom e!: "))
-alap: str = ""
-for cv in range(len(alap)-1, -1, -1):
-    print("Ez palindrom!")
-else:
-    print("Ez nem az!")
